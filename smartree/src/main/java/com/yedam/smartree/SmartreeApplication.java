8f862e3e1085805549956fddbba7c8e644367ee0
admin <admin@DESKTOP-9QDJ81V>
2023-09-11 10:44:29 +0900

    df

---
+++ smartree/src/main/java/com/yedam/smartree/SmartreeApplication.java
@@ -0,0 +1,13 @@
+package com.yedam.smartree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartreeApplication.class, args);
+	}
+
+}
---
+++ smartree/src/test/java/com/yedam/smartree/SmartreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yedam.smartree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
