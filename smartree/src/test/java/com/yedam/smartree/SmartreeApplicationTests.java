024cbffe9669afe8d74535030a9855f43004affd
admin <admin@DESKTOP-9QDJ81V>
2023-09-12 12:09:32 +0900

    ENC

--- smartree/src/test/java/com/yedam/smartree/SmartreeApplicationTests.java
+++ smartree/src/test/java/com/yedam/smartree/SmartreeApplicationTests.java
@@ -1,13 +1,26 @@
 package com.yedam.smartree;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SmartreeApplicationTests {
-
+	@Autowired
+	StringEncryptor jasyptStringEncryptor;
 	@Test
 	void contextLoads() {
+		String[] datas = {
+				"oracle.jdbc.OracleDriver"
+				,"jdbc:oracle:thin:@43.201.167.53/xe"
+				,"ttree"
+				,"ttree"
+		};
+		for(String data : datas) {
+			String enyData = jasyptStringEncryptor.encrypt(data);
+			System.out.println(enyData);
+		}
 	}
 
 }
