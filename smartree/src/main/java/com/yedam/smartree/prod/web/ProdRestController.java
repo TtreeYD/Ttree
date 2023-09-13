01970c003c24b4aa104b986fad0946d0539b627d
minseok <admin@DESKTOP-6AS9MCG>
2023-09-13 13:06:25 +0900

    민석 생산

---
+++ smartree/src/main/java/com/yedam/smartree/prod/web/ProdRestController.java
@@ -0,0 +1,19 @@
+package com.yedam.smartree.prod.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yedam.smartree.business.service.BusinessService;
+import com.yedam.smartree.business.service.BusinessVO;
+
+@RestController
+public class ProdRestController {
+//	@Autowired
+	
+	
+//	@GetMapping("/dtProdPlan")
+	
+}
