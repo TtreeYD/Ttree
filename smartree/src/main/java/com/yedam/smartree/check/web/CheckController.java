1bc5f84336a2e91444d7bb6448d1587d6a3869cc
admin <admin@DESKTOP-CKG3O77>
2023-09-13 17:40:46 +0900

    0913오후 jin

--- smartree/src/main/java/com/yedam/smartree/check/web/CheckController.java
+++ smartree/src/main/java/com/yedam/smartree/check/web/CheckController.java
@@ -3,9 +3,11 @@ package com.yedam.smartree.check.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.yedam.smartree.check.service.MtlCheckService;
 
+@RequestMapping("/check")
 @Controller
 public class CheckController {
 
