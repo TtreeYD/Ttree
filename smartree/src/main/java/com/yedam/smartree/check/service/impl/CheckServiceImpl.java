358f2d3ce46c35b55793b86cce6d75d2f3ede3ea
admin <admin@DESKTOP-9QDJ81V>
2023-09-13 10:05:58 +0900

    개발환경 마지막

--- smartree/src/main/java/com/yedam/smartree/check/service/impl/CheckServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.yedam.smartree.check.service.impl;
-
-import com.yedam.smartree.check.service.CheckService;
-
-public class CheckServiceImpl implements CheckService{
-
-}
--- smartree/src/main/java/com/yedam/smartree/material/mapper/MaterialMapper.java
+++
@@ -1,5 +0,0 @@
-package com.yedam.smartree.material.mapper;
-
-public class MaterialMapper {
-
-}
