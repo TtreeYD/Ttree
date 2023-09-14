016f8f6b87103f71ea3d2ec3831a064a2f988be4
minseok <admin@DESKTOP-6AS9MCG>
2023-09-14 17:49:36 +0900

    minseok

--- smartree/src/main/java/com/yedam/smartree/prod/mapper/ProdMapper.java
+++ smartree/src/main/java/com/yedam/smartree/prod/mapper/ProdMapper.java
@@ -4,10 +4,18 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.yedam.smartree.business.service.BusinessVO;
 import com.yedam.smartree.prod.service.ProdVO;
 
-@Mapper
 public interface ProdMapper {
 	
 	public List<ProdVO> selectProdList();
+
+	public List<BusinessVO> selectAllOrderAndPrdt();
+//	
+//	public List<BusinessVO> selectOrderAndPrdt(BusinessVO vo);
+	
+	public int insertProdPlan(ProdVO vo);
+	
+	public int insertDtProdPlan(ProdVO vo);
 }
--- smartree/src/main/java/com/yedam/smartree/prod/web/ProdRestController.java
+++ smartree/src/main/java/com/yedam/smartree/prod/web/ProdRestController.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.yedam.smartree.business.service.BusinessVO;
 import com.yedam.smartree.prod.service.ProdService;
 import com.yedam.smartree.prod.service.ProdVO;
+import com.yedam.smartree.prod.service.RequestVO;
 
 @RestController
 public class ProdRestController {
@@ -17,5 +22,30 @@ public class ProdRestController {
 	@GetMapping("/prodPlanList")
 	public List<ProdVO> getProdList(){
 		return service.selectProdList();
+	}
+	
+	@GetMapping("/orderAndPrdt")
+	public List<BusinessVO> getOrderAndPrdt(){
+		return service.selectOrderAndPrdt();
+	}
+//	
+//	@GetMapping("/OneOrderAndPrdt")
+//	public List<BusinessVO> getOneOrderAndPrdt(BusinessVO vo){
+//		System.out.println(vo.toString());
+//		return service.selectOneOrderAndPrdt(vo);
+//	}
+//	
+//	@PostMapping("/OneOrderAndPrdt")
+//	@ResponseBody
+//	public List<BusinessVO> getOneOrderAndPrdtResp(BusinessVO vo){
+////		System.out.println(vo.toString());
+//		return service.selectOneOrderAndPrdt(vo);
+//	}
+	
+	@PostMapping("/insertProdPlan")
+	@ResponseBody
+	public int insertProdPlan(@RequestBody RequestVO<ProdVO> vo) {
+		 return service.insertProdPlan(vo);
+		 
 	}
 }
