fbf2bf2cf5720e7e0a5349f4a256f275ee1a82c2
HH <HH@DESKTOP-VN13BF0>
2023-09-17 23:36:43 +0900

    정호현 영업관리 수정 삭제

--- smartree/src/main/java/com/yedam/smartree/business/service/BusinessService.java
+++ smartree/src/main/java/com/yedam/smartree/business/service/BusinessService.java
@@ -11,13 +11,26 @@ public interface BusinessService {
 	public List<BusinessVO> getOrder(BusinessVO businessVO);
 
 	//주문서등록
-	public int insertOrder(ReqVO<BusinessVO> businessvo);
+	public  int insertOrder(ReqVO<BusinessVO> businessvo);
 	
+	//주문서추가등록
+	public int plusDtOrder(ReqVO<BusinessVO> businessvo);
+	
+	//주문서삭제
+	public int deleteOrder(ReqVO<BusinessVO> businessvo);
+	
+	//주문서상세삭제
+	public int deleteDtOrder(ReqVO<BusinessVO> businessvo);
+	
+	//주문서수정
+	public int updateOrder(ReqVO<BusinessVO> businessvo);
 	
 	//업체목록
 	public List<BpVO> selectBpList(BpVO bpVO);
 	
 	//제품목록
 	public List<BpVO> selectPrdtList(BpVO bpVO);
+
+	
 
 }
