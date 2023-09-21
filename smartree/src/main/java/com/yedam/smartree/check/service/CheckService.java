1c5d67e34578753c155a74631dd6e2ca5c119bc5
admin <admin@DESKTOP-CKG3O77>
2023-09-21 12:33:25 +0900

    0921 12:30

--- smartree/src/main/java/com/yedam/smartree/check/service/CheckService.java
+++ smartree/src/main/java/com/yedam/smartree/check/service/CheckService.java
@@ -3,6 +3,8 @@ package com.yedam.smartree.check.service;
 import java.util.List;
 
 import com.yedam.smartree.business.service.BpVO;
+import com.yedam.smartree.business.service.BusinessVO;
+import com.yedam.smartree.business.service.ReqVO;
 
 public interface CheckService {
 	public List<BpVO> selectPrdtList(BpVO vo);
@@ -12,6 +14,7 @@ public interface CheckService {
 	//완제품 검사목록
 	public List<CheckVO> selectProdCheckList(CheckVO vo);
 	public List<CheckVO> selectProdDtList(CheckVO vo);
+	public int insertProdChk(ReqVO<CheckVO> vo);
 	
 	//완제품 검사조회목록
 	public List<CheckVO> selectPrdtCheckList(CheckVO vo);
