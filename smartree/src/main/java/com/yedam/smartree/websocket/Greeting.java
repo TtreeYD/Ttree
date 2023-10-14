53bf964249d6ee28330dbb19cfa84e90da07e09a
pc <pc@DESKTOP-LC5VKSQ>
2023-10-15 00:40:02 +0900

    0040

--- smartree/src/main/java/com/yedam/smartree/websocket/Greeting.java
+++ smartree/src/main/java/com/yedam/smartree/websocket/Greeting.java
@@ -1,16 +1,12 @@
 package com.yedam.smartree.websocket;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Greeting {
 	 private String content;
-
-	  public Greeting() {
-	  }
-
-	  public Greeting(String content) {
-	    this.content = content;
-	  }
-
-	  public String getContent() {
-	    return content;
-	  }
 }
--- smartree/src/main/java/com/yedam/smartree/websocket/GreetingController.java
+++ smartree/src/main/java/com/yedam/smartree/websocket/GreetingController.java
@@ -1,18 +1,22 @@
 package com.yedam.smartree.websocket;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class GreetingController {
+	
 	@MessageMapping("/hello")
 	@SendTo("/topic/greetings")
 	  public Greeting greeting(HelloMessage message) throws Exception {
-	    Thread.sleep(1000);
-	    System.out.println("==================================");
-	    return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+		String sender = message.getSender();
+	    Thread.sleep(300);
+	    return new Greeting(HtmlUtils.htmlEscape(sender + ": " + message.getName()));
 	  }
 	
 }
